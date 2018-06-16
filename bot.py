3091bb0be059c05be261d39506d5a2465e570f95
john <john@MacBook-Air-john.local>
2018-06-16 13:13:07 +0300

    added plotting

--- bot.py
+++ bot.py
@@ -8,8 +8,12 @@ import wolfram
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types = ["text"])
-def repeat_all_messages(message):
-    ans = wolfram.wolfram_req(message.text)
+def handle_equation(message):
+    if message.text.split()[0] == "plot":
+        keyword = "plot"
+    else:
+        keyword = "solution"
+    ans = wolfram.wolfram_req(keyword, message.text)
     if ans=="":
 	    bot.send_message(message.chat.id, "Sorry, I don't understand your equation :(")
     else:
@@ -17,6 +21,7 @@ def repeat_all_messages(message):
         for a in ans:
             # bot.send_message(message.chat.id, a)
             bot.send_document(message.chat.id, a)
+
 
 if __name__ == '__main__':
 	bot.polling(none_stop=True, timeout=100)
--- wolfram.py
+++ wolfram.py
@@ -6,20 +6,46 @@ client = wolframalpha.Client(config.key)
 # res = client.query("sin(x) + 2 cos(x) = -1")
 keyword = "solution"
 
-def wolfram_req(equation):
-    res = client.query(equation)
+def wolfram_req(keyword, message):
+    if keyword == "solution":
+        return wolfram_equation(message)
+    elif keyword == "plot":
+        return wolfram_plot(message)
+    else:
+        return ""
+
+def wolfram_equation(message):
+    keyword = "solution"
+    res = client.query(message)
     if int(res["@numpods"]) == 0:
         return ""
     pods_list = list(res.pods)
+    ans = []
     for pod in pods_list:
         if keyword in pod["@title"].lower():
-            ans = []
-            print(pod)
             if int(pod["@numsubpods"]) > 1:
                 for subpod in pod["subpod"]:
                     ans.append(subpod["img"]["@src"])
             elif int(pod["@numsubpods"]) > 0:
                 img = pod["subpod"]["img"]["@src"]
                 ans.append(img)
-            return ans
-    return ""
+    return ans if len(ans) else ""
+
+
+def wolfram_plot(message):
+    keyword = "plot"
+    res = client.query(message)
+    if int(res["@numpods"]) == 0:
+        return ""
+    pods_list = list(res.pods)
+    ans = []
+    for pod in pods_list:
+        if keyword in pod["@title"].lower():
+            
+            if int(pod["@numsubpods"]) > 1:
+                for subpod in pod["subpod"]:
+                    ans.append(subpod["img"]["@src"])
+            elif int(pod["@numsubpods"]) > 0:
+                img = pod["subpod"]["img"]["@src"]
+                ans.append(img)
+    return ans if len(ans) else ""
